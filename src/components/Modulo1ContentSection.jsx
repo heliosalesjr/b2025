4ea463c736870f3c1d50860dd0063c2692c8fe49
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-05-15 19:34:18 -0300

    added the fade in to the 1st 2 pages components

--- src/components/Modulo1ContentSection.jsx
+++ src/components/Modulo1ContentSection.jsx
@@ -1,3 +1,4 @@
+import FadeInWhenVisible from "./FadeInWhenVisible"
 import Modulo1Intro from "./content/Modulo1Intro"
 import Modulo1Objetivos from "./content/Modulo1Objetivos"
 import Modulo1Situacao from "./content/Modulo1Situacao"
@@ -12,12 +13,30 @@ export default function Modulo1ContentSection() {
         <div className="container mx-auto px-4 md:px-6">
           <div className="mx-auto max-w-5xl space-y-8">
             
-            
-                <Modulo1Intro />
-                <Modulo1Objetivos />
-                <Modulo1Situacao />
-                <Modulo1Situacao2 />
-                <Modulo1Video />
+                <FadeInWhenVisible>
+                  <Modulo1Intro />
+                </FadeInWhenVisible>
+                  
+                <FadeInWhenVisible>
+                  <Modulo1Objetivos />
+                </FadeInWhenVisible>
+
+                <FadeInWhenVisible>
+                  <Modulo1Situacao />
+                </FadeInWhenVisible>
+
+                <FadeInWhenVisible>
+                  <Modulo1Situacao2 />
+                </FadeInWhenVisible>
+
+                <FadeInWhenVisible>
+                  <Modulo1Video />
+                </FadeInWhenVisible>
+                
+                
+                
+                
+                
                 <div className="flex justify-center mt-8">
                     <Button asChild className={"bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"}>
                     <Link href="/educacaofinanceira">Próxima Página: Educação Financeira</Link>
